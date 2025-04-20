4ca95846952f21485e91f320a318fea127617cf3
fernandojosecc <fernandojosecc@hotmail.com>
2025-04-19 19:21:32 -0700

    Adding the button when the game is over

--- lib/gameoverscreen.dart
+++ lib/gameoverscreen.dart
@@ -1,19 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class GameOverScreen extends StatelessWidget {
   final bool isGameOver;
+  final function;
 
-  GameOverScreen({required this.isGameOver});
+  //font
+  static var gameFont = GoogleFonts.pressStart2p(
+    textStyle: TextStyle(
+      color: Colors.deepPurple[600],
+      letterSpacing: 0,
+      fontSize: 28,
+    ),
+  );
+
+  GameOverScreen({required this.isGameOver, this.function});
 
   @override
   Widget build(BuildContext context) {
     return isGameOver
-        ? Container(
-          alignment: Alignment(0, -0.3),
-          child: Text(
-            'G A M E   O V E R',
-            style: TextStyle(color: Colors.deepPurple),
-          ),
+        ? Stack(
+          children: [
+            Container(
+              alignment: Alignment(0, -0.3),
+              child: Text(
+                'G A M E   O V E R',
+                style: TextStyle(color: Colors.deepPurple),
+              ),
+            ),
+            Container(
+              alignment: Alignment(0, 0),
+              child: GestureDetector(
+                onTap: function,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    color: Colors.deepPurple,
+                    child: Text(
+                      'PLAY AGAIN',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         )
         : Container();
   }
